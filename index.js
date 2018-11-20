16af4a02be65351bed7fe07a462037d636d39056
mcumm64 <mcumm64@gmail.com>
2018-11-20 22:52:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {flour: "1 cup", eggs: "2"};
 
 function updateOpbjectWithKeyAndValue(object, key, value) {
 var addKeyVal = Object.assign({}, object, {[key]: value});
-  return addKeyVal;
+  return object, addKeyVal;
 }
 
